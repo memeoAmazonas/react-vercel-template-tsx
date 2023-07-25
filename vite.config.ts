cba6105ecf1059ad9720a7d0de0fe0bf7d2f6d11
Cantaclaro <41208420+memeoAmazonas@users.noreply.github.com>
2023-07-25 09:41:09 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { dependencies } from './package.json';
 import * as path from 'path';
@@ -6,26 +6,40 @@ import * as path from 'path';
 function renderChunks(deps: Record<string, string>) {
   const chunks = {};
   Object.keys(deps).forEach((key) => {
-    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    if (['react', 'react-router-dom', 'react-dom', 'react-file-viewer'].includes(key)) return;
     chunks[key] = [key];
   });
   return chunks;
 }
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
-  },
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-router-dom', 'react-dom'],
-          ...renderChunks(dependencies),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        { find: '~', replacement: path.resolve(__dirname, 'src') },
+        { find: './runtimeConfig', replacement: './runtimeConfig.browser' },
+      ],
+    },
+    build: {
+      sourcemap: false,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-router-dom', 'react-dom', 'react-file-viewer'],
+            ...renderChunks(dependencies),
+          },
         },
       },
     },
-  },
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      strictPort: true,
+      port: Number(env.VITE_SERVER_PORT) || 3000, // you can replace this port with any port
+    },
+  };
 });
